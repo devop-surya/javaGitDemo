fce5a4f69b73bf8173a2e4b4d1498459f4958194
Surya Prakash <131901801+devop-surya@users.noreply.github.com>
2023-08-17 19:53:42 +0530

    Update HelloCoder.java

--- javaGitDemo/src/com/demofile/HelloCoder.java
+++ javaGitDemo/src/com/demofile/HelloCoder.java
@@ -4,9 +4,8 @@ public class HelloCoder {
 	String name="Surya";
 	public static void main(String[] args) {
 		HelloCoder obj = new HelloCoder();
-		
 		System.out.println("Hello Developer...\nThis is Surya webPage "+obj.name);
-		
+		sysout("Thanks for visit our site");
 	}
 
 }
